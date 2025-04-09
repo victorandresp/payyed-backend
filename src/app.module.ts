fc9b13f77644c826671454b4579b4caff5b4420d
victorandresp <ponceottamendi27@gmail.com>
2025-04-08 22:44:08 -0400

    chore: installed mongoose :wrench:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      // envFilePath: './.env',
+    }),
+    MongooseModule.forRoot(process.env.MONGO_DB_URI as string),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
