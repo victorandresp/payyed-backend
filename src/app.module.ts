6a2b6622fe88583fbe98568eaeb5e3382f187ec5
victorandresp <ponceottamendi27@gmail.com>
2025-04-09 22:35:39 -0400

    chore: rules for commas :wrench

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,17 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true,
+      isGlobal: true
       // envFilePath: './.env',
     }),
     MongooseModule.forRoot(process.env.MONGO_DB_URI as string),
+    UserModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
