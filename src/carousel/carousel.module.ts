b71537547aae36e532548f12adcd49f4eacb7dde
victorandresp <ponceottamendi27@gmail.com>
2025-04-26 22:55:23 -0400

    feat(carousel): module updated providers :sparkles:

--- src/carousel/carousel.module.ts
+++ src/carousel/carousel.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
-import { CarouselService } from './carousel.service';
-import { Carousel, CarouselSchema } from './carousel.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Carousel, CarouselSchema } from './carousel.schema';
+import { CarouselResolver } from './carousel.resolver';
+import { CarouselService } from './carousel.service';
+import { CarouselRepository } from './carousel.repository';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Carousel.name, schema: CarouselSchema }])
   ],
-  providers: [CarouselService]
+  providers: [CarouselResolver, CarouselService, CarouselRepository]
 })
 export class CarouselModule {}
