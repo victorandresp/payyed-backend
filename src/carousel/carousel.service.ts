ea025ce8f69676981391997531c3e31583140bb2
victorandresp <ponceottamendi27@gmail.com>
2025-04-26 22:54:21 -0400

    feat(carousel): service layer + query for getImages :sparkles:

--- src/carousel/carousel.service.ts
+++ src/carousel/carousel.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
-
+import { CarouselRepository } from './carousel.repository';
 @Injectable()
-export class CarouselService {}
+export class CarouselService {
+  constructor(private readonly carouselRepository: CarouselRepository) {}
+
+  async getCarouselImages() {
+    return this.carouselRepository.getCarouselImages();
+  }
+}
