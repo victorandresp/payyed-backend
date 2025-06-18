4ca0086512d526949806311f0b6166a8bb3b56e5
victorandresp <ponceottamendi27@gmail.com>
2025-06-17 21:57:06 -0400

    feat(auth): added repository layer dependencie structure :sparkles:

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
+import { AuthRepository } from './auth.repository';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, AuthResolver]
+  providers: [AuthService, AuthResolver, AuthRepository]
 })
 export class AuthModule {}
