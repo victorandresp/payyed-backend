61b6c8091ae8d7317135b3f92d7ad544a417ce0f
victorandresp <ponceottamendi27@gmail.com>
2025-06-17 22:01:10 -0400

    fix(auth): removed controller from layer :bug:

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 import { AuthRepository } from './auth.repository';
 
 @Module({
-  controllers: [AuthController],
   providers: [AuthService, AuthResolver, AuthRepository]
 })
 export class AuthModule {}
