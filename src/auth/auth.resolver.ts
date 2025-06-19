5ec4d3aea49f1a91d7d161bfd7a6bdc27e886ebf
victorandresp <ponceottamendi27@gmail.com>
2025-06-18 22:50:38 -0400

    feat(auth): resolver structure principal :sparkles:

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,5 +1,14 @@
-// import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Args } from '@nestjs/graphql';
+import { UserType } from '../user/user.type';
+import { AuthService } from './auth.service';
 
 export class AuthInput {}
 
-export class AuthResolver {}
+@Resolver(() => UserType)
+export class AuthResolver {
+  constructor(private readonly authService: AuthService) {}
+  @Query(() => UserType)
+  logIn(@Args('email') email: string, @Args('password') password: string) {
+    return this.authService.logIn(email, password);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
-
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  logIn(email: string, password: string) {
+    return {
+      firstName: `login does works! ${email} ${password}`
+    };
+  }
+}
