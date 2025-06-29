e803dd3589dc0b99eda5196ce93f31f49e579eec
victorandresp <ponceottamendi27@gmail.com>
2025-06-28 21:39:04 -0400

    feat(Auth): repository updated, injected user model and query for check email in db :sparkles:

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -1,3 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from 'src/user/user.schema';
+
 @Injectable()
-export class AuthRepository {}
+export class AuthRepository {
+  constructor(
+    @InjectModel(User.name)
+    private readonly userModel: Model<UserDocument>
+  ) {}
+  async emailExists(email: string) {
+    return await this.userModel.findOne({ email });
+  }
+}
