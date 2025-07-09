c8c0ee28586468e9c8d5d4ba97d0a27bbe295e97
victorandresp <ponceottamendi27@gmail.com>
2025-07-08 22:46:24 -0400

    feat: added mongo sanitize queries :lock:

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import * as mongoSanitize from 'express-mongo-sanitize';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,7 @@ async function bootstrap() {
     credentials: true
   });
   app.use(helmet());
+  app.use(mongoSanitize());
   await app.listen(process.env.PORT ?? 3000);
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
