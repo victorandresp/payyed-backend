c000ae77cf10226b74a12cb99ce2aa59525d43c8
victorandresp <ponceottamendi27@gmail.com>
2025-07-08 22:08:28 -0400

    feat: added helmet :lock:

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,6 +8,7 @@ async function bootstrap() {
     origin: [process.env.FRONTEND_URL],
     credentials: true
   });
+  app.use(helmet());
   await app.listen(process.env.PORT ?? 3000);
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
