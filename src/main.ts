e20ab36aa3337a4ad37fca653e679b234475f592
victorandresp <ponceottamendi27@gmail.com>
2025-07-20 14:09:05 -0400

    chore: added validation for dev enviroment for allow cors and helmet security layer :wrench:

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
-import * as mongoSanitize from 'express-mongo-sanitize';
+// import * as mongoSanitize from 'express-mongo-sanitize';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [process.env.FRONTEND_URL],
-    credentials: true
-  });
-  app.use(helmet());
-  app.use(mongoSanitize());
+
+  if (process.env.NODE_ENV === 'production') {
+    app.enableCors({
+      origin: [process.env.FRONTEND_URL],
+      credentials: true
+    });
+    app.use(helmet());
+  }
+  // app.use(mongoSanitize());
   await app.listen(process.env.PORT ?? 3000);
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
