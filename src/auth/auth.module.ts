375d9923dfdd973644aacfd35958a9b26f9ce13a
victorandresp <ponceottamendi27@gmail.com>
2025-07-22 22:18:31 -0400

    chore(Auth): config async module load for JWT strategy :wrench:

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService, ConfigModule } from '@nestjs/config';
+import { JwtStrategy } from '../jwt/jwt.strategy';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 import { AuthRepository } from './auth.repository';
 import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [UserModule],
-  providers: [AuthService, AuthResolver, AuthRepository]
+  imports: [
+    PassportModule,
+    UserModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: config.get<string>('JWT_EXPIRE_TIME') }
+      }),
+      inject: [ConfigService]
+    })
+  ],
+  providers: [AuthService, AuthResolver, AuthRepository, JwtStrategy]
 })
 export class AuthModule {}
