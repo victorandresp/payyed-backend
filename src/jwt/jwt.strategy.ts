42fbd88f0c1b58f6a029bb913509d0ada0e222fb
victorandresp <ponceottamendi27@gmail.com>
2025-07-22 22:16:05 -0400

    feat(JWT): module for JWT :sparkles:

---
+++ src/jwt/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private configService: ConfigService) {
+    const secret = configService.get<string>('JWT_SECRET');
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: secret as string
+    });
+  }
+
+  validate(payload: { email: string; id: string }) {
+    return { id: payload.id, email: payload.email };
+  }
+}
