a8c10c378728e02e06d18a552a05412a97d35de5
victorandresp <ponceottamendi27@gmail.com>
2025-07-22 20:32:48 -0400

    fix(Auth): changed query to mutation in logIn resolver :bug:

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,11 +1,11 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { Resolver, Mutation, Args } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
 import { LogInArgs, LogInOutput, SignInInput } from './auth.types';
 import { UserType } from '../user/user.types';
 @Resolver(() => LogInOutput)
 export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
-  @Query(() => LogInOutput) // TODO: pass to mutation
+  @Mutation(() => LogInOutput) // TODO: pass to mutation
   logIn(@Args() args: LogInArgs) {
     const { email, password } = args;
     return this.authService.logIn(email, password);
