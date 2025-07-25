58ad0ca2fcd8745a9b88398e20311e044cb1f760
victorandresp <ponceottamendi27@gmail.com>
2025-07-24 21:28:09 -0400

    feat: validate resolver for get carousels (test) :sparkles:

--- src/carousel/carousel.resolver.ts
+++ src/carousel/carousel.resolver.ts
@@ -1,11 +1,13 @@
 import { Resolver, Query } from '@nestjs/graphql';
 import { CarouselType } from './carousel.types';
 import { CarouselService } from './carousel.service';
+import { Authenticated } from '../auth/auth.decorator';
 @Resolver(() => CarouselType)
 export class CarouselResolver {
   constructor(private readonly carouselService: CarouselService) {}
 
   @Query(() => [CarouselType])
+  @Authenticated()
   async getCarouselImages() {
     return this.carouselService.getCarouselImages();
   }
